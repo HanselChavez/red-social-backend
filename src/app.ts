aca78e50e223de38afd428b0254b7c944d4586d6
HanselChavez <N00274924@upn.pe>
2026-04-28 22:37:47 -0500

    modificando allow origin para CORS

--- src/app.ts
+++ src/app.ts
@@ -14,18 +14,23 @@ console.log("DB_HOST:", process.env.DB_HOST);
 //   credentials: true
 // }))
 
-const allowedOrigins = ["*",process.env.FRONTEND_URL];
+const allowedOrigins = [
+  "http://localhost:5173",
+  process.env.FRONTEND_URL,
+];
 app.use(
-    cors({
-        origin: function (origin, callback) {
-            if (!origin || allowedOrigins.includes(origin)) {
-                callback(null, true);
-            } else {
-                callback(new Error("Not allowed by CORS"));
-            }
-        },
-        credentials: true,
-    }),
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true); 
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
+    credentials: true,
+  })
 );
 app.use(express.json());
 
